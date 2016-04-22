d683d98e3a3a5157b803b5a01227fd736226760d
Simon Hylander <simon.hylander@docu.se>
2016-04-23 01:11:04 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,26 +1,8 @@
 'use strict';
 
-angular.module('chatApp', [,
-    'ui.bootstrap', 'ui.router',
-]);
-    /*
-    .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider,  httpRequestInterceptorCacheBusterProvider, AlertServiceProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider.state('site', {
-            'abstract': true,
-            views: {
-                'navbar@': {
-                    templateUrl: 'scripts/components/navbar/navbar.html',
-                    controller: 'NavbarController'
-                }
-            },
-            resolve: {
-                authorize: ['Auth',
-                    function (Auth) {
-                        return Auth.authorize();
-                    }
-                ]
-            }
-        });
-
-    })*/
+var app = angular.module('chatApp', []);
+app.controller('chatController', function($scope){
+    $scope.sendMessage = function() {
+        console.log('send message');
+    }
+});
